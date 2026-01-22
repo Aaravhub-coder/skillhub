406bddf23714b3713bd3e4ad3f6eb0c2bebdf2b5
Aarav raj <aaravrajgo@gmail.com>
2026-01-22 23:21:30 +0530

    merge commit

--- skillhub.py
+++ skillhub.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask,render_template
+
 app = Flask(__name__)
-@app.route("/")
-def index():
-    return "<p>this is second index</p>"
+
+
+@app.route('/')
+def index2():
+    return render_template("index.html")
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/aaravraj/Desktop/aaravr/venv"
+set -gx VIRTUAL_ENV "/Users/aaravraj/Desktop/aaravr1/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
